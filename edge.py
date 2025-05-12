16676409d5648a9d654ce5100ba2c3d0cdefe759
Kouki Hagiwara <kouki-abc-1108@outlook.jp>
2025-05-12 17:19:12 +0900

    edgeの変更

---
+++ edge.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import cv2
+import numpy as np
+
+# トラックバーのコールバック関数（何もしない）
+def nothing(x):
+    pass
+
+# ウィンドウ作成とトラックバー(GUIスライダー)追加
+cv2.namedWindow('Canny Edge')
+
+cv2.createTrackbar("Threshold1", "Canny Edge", 50, 500, nothing)
+cv2.createTrackbar("Threshold2", "Canny Edge", 150, 500, nothing)
+
+def canny_edge_demo():
+    # カメラ起動（0はデフォルトカメラ）
+    # 2がウェブカメラ
+    #カメラから映像を読み込む
+    #解像度を640×480に設定
+    cap = cv2.VideoCapture(2)
+    cap.set(3, 640) #幅
+    cap.set(4, 480) #高さ
+
+    #映像フレームごとに処理
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # トラックバーからしきい値を取得(thはトラックバー)
+        th1 = cv2.getTrackbarPos("Threshold1", "Canny Edge")
+        th2 = cv2.getTrackbarPos("Threshold2", "Canny Edge")
+
+        # グレースケール化(これで白黒にしてる)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Cannyエッジ検出(輪郭を検出)
+        edges = cv2.Canny(gray, th1, th2)
+
+        # 表示
+        cv2.imshow("Original", frame)
+        cv2.imshow("Canny Edge", edges)
+
+        # ESCキーで終了
+        k = cv2.waitKey(10)
+        if k == 27:  # ESC
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    canny_edge_demo()
